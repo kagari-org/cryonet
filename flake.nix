ba1e1129bd53b7695b0c48469a5306a350422aed
Anillc <i@anillc.cn>
2025-06-01 07:53:33 +0800

    add package to flake

--- flake.nix
+++ flake.nix
@@ -3,10 +3,30 @@
     self, nixpkgs, flake-parts,
   }: flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-    perSystem = { pkgs, ... }: {
+    perSystem = { pkgs, self', ... }: let
+
+      cryonet = { lib, rustPlatform, openssl, pkg-config, ... }: rustPlatform.buildRustPackage {
+        name = "cryonet";
+        RUSTC_BOOTSTRAP = true;
+        cargoLock.lockFile = ./Cargo.lock;
+        buildInputs = [ openssl ];
+        nativeBuildInputs = [ pkg-config ];
+        src = with lib.fileset; toSource {
+          root = ./.;
+          fileset = unions [
+            ./src
+            ./Cargo.toml
+            ./Cargo.lock
+          ];
+        };
+      };
+
+    in {
+      packages.default = pkgs.callPackage cryonet {};
+      packages.static = pkgs.pkgsStatic.callPackage cryonet {};
       devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ openssl pkgsStatic.openssl ];
-        nativeBuildInputs = with pkgs; [ pkg-config ];
+        RUSTC_BOOTSTRAP = true;
+        inputsFrom = [ self'.packages.default ];
       };
     };
   };
