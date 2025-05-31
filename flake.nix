9e7003ddb8f08135fe58d8af0122a5e2f71df480
Anillc <i@anillc.cn>
2025-05-31 09:44:47 +0800

    fix static linking

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     perSystem = { pkgs, ... }: {
       devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ openssl ];
+        buildInputs = with pkgs; [ openssl pkgsStatic.openssl ];
         nativeBuildInputs = with pkgs; [ pkg-config ];
       };
     };
